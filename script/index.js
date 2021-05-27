697cbb36e13e76541a690fec5b3cb464f4f70af4
Fialkaksu <fialkaksu1984@gmail.com>
2021-05-27 19:52:01 +0300

    fix: fix some moments

--- script/index.js
+++ script/index.js
@@ -19,31 +19,14 @@ const swiperWonders = new Swiper(".wonders-swiper", {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  // Responsive breakpoints
   breakpoints: {
-    // when window width is >= 320px
     320: {
-      // slidesPerView: 2,
       spaceBetween: 16
     },
-    // when window width is >= 640px
-    // 640: {
-    //   slidesPerView: 3,
-    //   spaceBetween: 15.5
-    // },
-    // when window width is >= 1200px
     1200: {
-      // slidesPerView: 3,
       spaceBetween: 24
     },
-    // when window width is >= 1920px
-    // 1920: {
-    //   slidesPerView: 3,
-    //   spaceBetween: 26
-    // }
   },
-  // centeredSlides: true,
-  // centeredSlidesBounds: true,
   initialSlide: 2,
   loop: true,
 });
@@ -52,4 +35,4 @@ const slides = document.querySelectorAll('.wonders-swiper .slide-img');
 
 Array.from(slides).forEach(slide => {
   slide.style.backgroundImage = `url("assets/slide${slide.dataset.count}.jpg")`
-})
+});
